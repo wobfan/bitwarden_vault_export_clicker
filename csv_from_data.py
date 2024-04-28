7f17ce637e4e44fde0b847724e3f9786f1f33522
Stefan Wobbe <wobfan@pm.me>
2024-04-28 22:46:59 +0200

    adapt csv converter

--- csv_from_data.py
+++ csv_from_data.py
@@ -11,13 +11,12 @@ for x in l:
     unique_l.append(x)
     names.append(x[0])
 
-def write_to_csv(data):
-    headers = ["name", "login_username", "login_password", "login_uri"]
+headers = ["name", "login_username", "login_password", "login_uri"]
 
-    with open('data.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(headers)
+with open('data.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(headers)
 
-        for row in data:
-            writer.writerow(row)
+    for row in unique_l:
+        writer.writerow(row)
     
